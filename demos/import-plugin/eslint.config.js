2b7bc2d614db251be4f26fc5a957765744e1c64e
Raul Melo <melo.raulf@gmail.com>
2023-07-19 09:30:23 +0200

    add import plugin demo

---
+++ demos/import-plugin/eslint.config.js
@@ -0,0 +1,16 @@
+import jsdoc from "eslint-plugin-jsdoc";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["*.js"],
+    plugins: {
+      jsdoc,
+    },
+    rules: {
+      // Other rules...
+      // JSDoc
+      "jsdoc/require-example": "error",
+    },
+  },
+];
---
+++ demos/import-plugin/index.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param {String} name
+ * @returns String
+ */
+function printName(name) {
+  return `Hello ${name}`;
+}
