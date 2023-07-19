3fa016d56296f85d53d493154e40815d4a5f7e16
Raul Melo <melo.raulf@gmail.com>
2023-07-19 09:13:52 +0200

    create airbnb base compat problem

---
+++ demos/airbnb-base-compat-problem/eslint.config.js
@@ -0,0 +1,13 @@
+import airbnb from "eslint-config-airbnb-base";
+
+/**
+ * This won't work, at least not in the versions used in this demo.
+ *
+ * Error:
+ *
+ * Oops! Something went wrong! :(
+ * ESLint: 8.45.0
+ * Error: Unexpected key "extends" found.
+ */
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [airbnb];
