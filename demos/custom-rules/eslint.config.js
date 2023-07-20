4b738470d067d1a2ea2cb39a026f3168fd8f95a8
Raul Melo <melo.raulf@gmail.com>
2023-07-20 06:27:06 +0200

    add custom rule to eslint flat config

--- demos/custom-rules/eslint.config.js
+++ demos/custom-rules/eslint.config.js
@@ -1,9 +1,19 @@
+const noDoubleUnderscore = require("./eslint/rules/no-double-underscore-variable");
+
 /** @type {import('eslint').Linter.FlatConfig[]} */
 module.exports = [
   {
+    plugins: {
+      internal: {
+        rules: {
+          "no-double-underscore-variable": noDoubleUnderscore,
+        },
+      },
+    },
     rules: {
       semi: "error",
       "prefer-const": "error",
+      "internal/no-double-underscore-variable": "error",
     },
   },
 ];
